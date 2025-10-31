fa8444d189551f78035ee57cc401e1f936757b52
NAS-Ragde <nayeli.anaid.silva@gmail.com>
2025-10-31 11:25:22 +0100

    fix: fix and recover info_to_yaml function.

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
-import csv
 import os
+import re
 import yaml
 import sqlite3
 import pandas as pd
 from filtering import assessment_window_filtering, filtering_interesting_ids, filtering_excluded_ids
 from utils import load_config_file, write_config_file, detect_separator
-import re
 
 
 db_filepath = load_config_file('DB', 'current_db')
 baseline_ids_directory = load_config_file('filters', 'baseline_ids_directory')
 
 # --- Record release: Record_00 (test)
-filepath_requirements_id_00 = load_config_file('data_requirements', 'input_release_num_22')
-filepath_release_id_00 = load_config_file('data_release', 'output_release_num_22')
+filepath_requirements_id_00 = load_config_file('data_requirements', 'input_release_num_00')
+filepath_release_id_00 = load_config_file('data_release', 'output_release_num_00')
 additional_ids_filter_directory_id_00 = load_config_file('filters', 'additional_id_filter_num_22')
 
 
@@ -70,6 +69,7 @@ def prepare_tables_to_export(file_map):
             variables_to_add = [v for v in variables_to_add if isinstance(v, str)]
 
         for table in table_names:
+            print(table_names)
             if table not in all_tables:
                 print(f"Warning: Table '{table}' not found in DB. Skipping.")
                 continue
@@ -121,13 +121,13 @@ def export_sqlite_tables_to_csv(file_map, output_dir):
 
 
 def create_participants_summary_from_df(output_path):
+    print("\nPreparing participants summary...")
     unique_values = {}
     value_columns = ["unit", "condition", "randomize"]
     # value_columns = ["participant_number", "VisitCode", "SiteCode"]  # --> only for MovisensESM
 
     for file in os.listdir(output_path):
         if file.endswith(".csv") and file.startswith("ITEM"):
-            print("Processing file:", file)
             filepath = os.path.join(output_path, file)
             separator = detect_separator(filepath)
             current_df = pd.read_csv(filepath, sep=separator, quotechar='"')
@@ -144,18 +144,95 @@ def create_participants_summary_from_df(output_path):
     output_filename = f'participants_conditions_summary.csv'
     output_file = os.path.join(os.path.dirname(output_path), output_filename)
     unique_participants_df.to_csv(output_file, sep=';', index=False)
-    print(f"Exported {len(unique_participants_df)} unique IDs to {output_file}")
+    print(f"Exported {len(unique_participants_df)} unique IDs in:\n {output_file}")
 
 
 def remove_header_from_csv(input_csv_path):
     for file in os.listdir(input_csv_path):
         if file.startswith("ITEM") and file.endswith(".csv"):
             filepath = os.path.join(input_csv_path, file)
-            print(f"Removing header from {file}, {filepath}")
+            print(f"Removing header from {file}")
 
             df = pd.read_csv(filepath, sep=";")
             new_filepath = os.path.join(input_csv_path, f"{file.replace(".csv", "_")}no_headers.csv")
             df.to_csv(new_filepath, sep=";", index=False, header=False)
+
+
+def info_to_yaml(info_txt_file_path):
+    info_path = os.path.join(info_txt_file_path, "info.txt")
+    file_data = None
+
+    try:
+        with open(info_path, 'r') as file:
+            file_data = file.read()
+    except FileNotFoundError:
+        print("The info.txt was not found in {}.".format(info_txt_file_path))
+
+    if file_data is None:
+        raise FileNotFoundError(f"No data loaded from {info_path}")
+
+    pattern = r"REQUEST: Record ID (\d+)\n\n(.*?)\Z"
+    interested_var1 = r"INTERESTED_VARIABLES:\s*\[([^\]]*)\]|"
+    interested_var2 = r"^\s*INTERESTED_VARIABLES:\s*\n((?:[ \t]+.+\n?)*)"
+    interested_vars = rf"{interested_var1}|{interested_var2}"
+    assessment_vars = r"-\s*Data Phase II assessment window:\s*([^\n]+)"
+
+    match = re.search(pattern, file_data, re.DOTALL)
+    if match:
+        record_id = int(match.group(1))  # Convert to integer
+        items_text = match.group(2)
+
+        data_dict = {"files": []}
+
+        # Detect assessment window to filter data
+        match_assessment = re.search(assessment_vars, items_text, re.DOTALL)
+        if match_assessment:
+            assessment_raw_values = match_assessment.group(1)
+            assessment_values = [v.strip() for v in assessment_raw_values.split(",") if v.strip()]
+            if assessment_values:
+                data_dict["assessment_window"] = assessment_values
+
+        item_blocks = [block for block in items_text.split('ITEM ') if block.strip()]
+
+        for item in item_blocks:
+            item_number_match = re.search(r"(\d+):", item)
+            if item_number_match:
+                item_number = int(item_number_match.group(1))
+            else:
+                continue  # Skip this item if no item number is found
+
+            csv_filenames_match = re.findall(r"([^\s]+\.csv)", item)
+            csv_filenames = [filename.replace('.csv', '') for filename in csv_filenames_match]
+
+            interested_variables_matches = re.findall(interested_vars, item, flags=re.MULTILINE)
+            interested_variables = []
+            for match1, match2 in interested_variables_matches:
+                captured = match1 or match2
+                if not captured:
+                    continue
+                if ',' in captured:
+                    interested_variables.extend([v.strip() for v in captured.split(',') if v.strip()])
+                else:
+                    interested_variables.extend([v.strip() for v in captured.splitlines() if v.strip()])
+
+            item_data = {
+                "item": item_number,
+                "name": csv_filenames or []
+            }
+
+            if interested_variables:
+                item_data["variables"] = interested_variables
+
+            data_dict["files"].append(item_data)
+
+        # bring into yaml-format & save
+        file_path = os.path.join(info_txt_file_path, f'request_id_{record_id}.yaml')
+        with open(file_path, 'w') as file:
+            yaml.dump(data_dict, file, default_flow_style=False, sort_keys=False)
+        saved_path = write_config_file(os.path.dirname(file_path), os.path.basename(file_path))
+
+        if file_path:
+            print(f"Info.txt written to request_id_{record_id}.yaml created at: {saved_path}")
 
 
 def main():
@@ -184,7 +261,7 @@ def main():
 
     # Step 8: Pseudo
 
-    # Step 9: Exports a copy of CSV files without headers.
+    # # Step 9: Exports a copy of CSV files without headers.
     remove_header_from_csv(filepath_release_id_00)
 
 
